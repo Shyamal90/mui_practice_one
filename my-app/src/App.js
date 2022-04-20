3ad0178cad4a3d71b63a611da798eddfaf0f89bc
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-04-20 09:44:17 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar/Navbar'
+import './App.css';
+import { Box } from '@mui/system';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Box>
+        
+      </Box>
+    </div>
+  );
+}
+
+export default App;
